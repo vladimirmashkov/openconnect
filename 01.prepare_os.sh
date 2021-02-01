f649906a5f38dc19185cadcb56769232bf53141a
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2021-02-01 20:59:13 +0300

    update

--- 01.prepare_os.sh
+++ 01.prepare_os.sh
@@ -7,16 +7,17 @@ sudo cp -f "etc/resolv.conf" "/etc/resolv.conf"
 
 mkdir -p -m 0644 /root/.ssh/
 wget https://github.com/vladimirmashkov.keys -O "/root/.ssh/mashkov_key.pub"
-cat /root/.ssh/mashkov_key.pub >> /root/.ssh/authorized_keys
+cat /root/.ssh/mashkov_key.pub > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
 
 sudo groupadd vpn_admin
 sudo useradd -m albert -G vpn_admin
 mkdir -p -m 0644 /home/albert/.ssh/
-cat etc/albert_key.pub >> /home/albert/.ssh/authorized_keys
-chmod -R 0644 /home/albert
-chown -R albert /home/albert
-chgrp -R albert /home/albert
-
+chmod 700 /home/albert/.ssh/
+touch /home/albert/.ssh/authorized_keys
+cat etc/albert_key.pub > /home/albert/.ssh/authorized_keys
+chmod 600 /home/albert/.ssh/authorized_keys
+chown albert:albert /home/albert/.ssh/ -R
 
 sed -i "s/PasswordAuthentication yes/#PasswordAuthentication yes/g" /etc/ssh/sshd_config
 cat etc/sshd_config >> /etc/ssh/sshd_config
