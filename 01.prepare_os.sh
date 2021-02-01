7ae13211fbf0b610ad96c6afb42f3479bbd01ae4
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2021-02-01 19:44:11 +0300

    update

--- 01.prepare_os.sh
+++ 01.prepare_os.sh
@@ -5,3 +5,19 @@ sudo yum install -y ntp ntpdate wget mc p7zip-full whois net-tools traceroute mt
 sudo ntpdate -s time.nist.gov
 sudo cp -f "etc/resolv.conf" "/etc/resolv.conf"
 
+mkdir -p -m 0644 /root/.ssh/
+wget https://github.com/vladimirmashkov.keys -P /root/.ssh/ -O "mashkov_key.pub"
+cat /root/.ssh/mashkov_key.pub >> /home/albert/.ssh/authorized_keys
+
+sudo groupadd vpn_admin
+sudo useradd -m albert -G vpn_admin
+mkdir -p -m 0644 /home/albert/.ssh/
+cat etc/albert_key.pub >> /home/albert/.ssh/authorized_keys
+chmod -R 0644 /home/albert
+chown -R albert /home/albert
+chgrp -R albert /home/albert
+
+
+sed -i "s/PasswordAuthentication yes/#PasswordAuthentication yes/g" /etc/ssh/sshd_config
+cat etc/sshd_config >> /etc/ssh/sshd_config
+sudo systemctl restart sshd
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # openconnect
+sed -i "s/SELINUX=permissive/SELINUX=disabled/g" /etc/selinux/config
+reboot <br/>
 
 yum install -y git <br/>
 git clone https://github.com/vladimirmashkov/openconnect.git <br/>
 cd openconnect/ <br/>
 
-git checkout origin/dev/centos7 <br/>
+git checkout origin/dev/centos7_ssl_admin <br/>
 
 sh 01.prepare_os.sh <br/>
 sh 02.prepare_soft.sh <br/>
 sh 03.install_soft.sh <br/>
 sh 04.cp_and_clean-up.sh <br/>
 sh 05.start_soft.sh <br/>
+reboot <br/>
 
 ocpasswd -c /etc/ocserv/passwd -g default my_login_name <br/>
