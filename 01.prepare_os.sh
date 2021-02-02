0bf4dba338e52dcd86916b5e3e174d2aa935f15d
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2021-02-02 23:33:38 +0300

    Update 01.prepare_os.sh

--- 01.prepare_os.sh
+++ 01.prepare_os.sh
@@ -13,5 +13,7 @@ yum install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl start docker
+systemctl enable docker
 
 yum update -y && yum upgrade -y
