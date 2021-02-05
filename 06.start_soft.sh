ca881f485e958625ca8d122ba7c5a2998f00f36c
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2021-02-05 03:57:07 +0300

    update

--- 06.start_soft.sh
+++ 06.start_soft.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 csf -r
 sysctl -p
+
+
+systemctl stop docker
+systemctl start docker
+systemctl status docker
--- docker_ocserv.sh
+++ docker_ocserv.sh
@@ -1,5 +1,10 @@
 docker run --name ocserv --privileged \
-    -p 4443:4443 \
-    -p 4443:4443/udp \
+    -p "4443:4443" \
+    -p "4443:4443/udp" \
     -v "/root/openconnect/ocserv:/etc/ocserv" \
+    -d tommylau/ocserv
+
+
+docker run --name ocserv --privileged \
+    -p "443:443" \
     -d tommylau/ocserv
