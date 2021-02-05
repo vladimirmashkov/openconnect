5d0050ab3aa39d82278b6e526db2d384e64eea09
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2021-02-05 03:29:49 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-01.prepare_os.sh
-02.prepare_scripts.sh
-03.prepare_soft.sh
-04.install_soft.sh
-05.cp_and_clean-up.sh
-06.start_soft.sh
+sh 01.prepare_os.sh
+sh 02.prepare_scripts.sh
+sh 03.prepare_soft.sh
+sh 04.install_soft.sh
+sh 05.cp_and_clean-up.sh
+sh 06.start_soft.sh
