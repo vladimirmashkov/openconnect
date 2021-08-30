b6ed50485499c454d06a4b5633bf93bc11ae2934
Vladimir Mashkov <vladimirmashkov@users.noreply.github.com>
2021-08-30 18:45:43 +0300

    Update 01.prepare_os.sh

--- 01.prepare_os.sh
+++ 01.prepare_os.sh
@@ -7,13 +7,13 @@ yum -y install --enablerepo=epel iptables-services wget perl unzip net-tools per
 ntpdate -s time.nist.gov
 
 echo "========== Install 7zip =========="
-wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-16.02-10.el7.x86_64.rpm
-wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-plugins-16.02-10.el7.x86_64.rpm
-rpm -U --quiet p7zip-16.02-10.el7.x86_64.rpm
-rpm -U --quiet p7zip-plugins-16.02-10.el7.x86_64.rpm
+#wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-16.02-10.el7.x86_64.rpm
+#wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-plugins-16.02-10.el7.x86_64.rpm
+#rpm -U --quiet p7zip-16.02-10.el7.x86_64.rpm
+#rpm -U --quiet p7zip-plugins-16.02-10.el7.x86_64.rpm
 
-rm -f p7zip-16.02-10.el7.x86_64.rpm
-rm -f p7zip-plugins-16.02-10.el7.x86_64.rpm
+#rm -f p7zip-16.02-10.el7.x86_64.rpm
+#rm -f p7zip-plugins-16.02-10.el7.x86_64.rpm
 
 cp -f "etc/resolv.conf" "/etc/resolv.conf"
 echo "========== Install Docker =========="
