f99b169309129226a4edb270f8d438c4799d88e2
Vladimir Mashkov <vladimir@mashkov.com>
2022-04-12 19:40:46 +0300

    Create 00.prepare_os_check.sh

---
+++ 00.prepare_os_check.sh
@@ -0,0 +1,34 @@
+###!/bin/bash
+yum update -y && yum upgrade -y
+yum install -y epel-release && \
+timedatectl set-timezone Europe/Moscow
+yum install -y --enablerepo=epel ntp ntpdate wget mc whois net-tools traceroute mtr sed tree
+yum -y install --enablerepo=epel iptables-services wget perl unzip net-tools perl-libwww-perl perl-LWP-Protocol-https perl-GDGraph
+ntpdate -s time.nist.gov
+
+echo "========== Install Docker =========="
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+
+echo "========== Install Docker Compose =========="
+curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl start docker
+systemctl enable docker
+
+sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+
+yum update -y && yum upgrade -y
+
+
+echo "version: '3.8'" > docker-compose.yaml
+echo "services:" >> docker-compose.yaml
+echo "  nginx:" >> docker-compose.yaml
+echo "    image: nginx:latest" >> docker-compose.yaml
+echo "    restart: always" >> docker-compose.yaml
+echo "    container_name: nginx" >> docker-compose.yaml
+
+docker-compose up -d
+
