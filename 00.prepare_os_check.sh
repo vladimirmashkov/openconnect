48523c5b14cf8be1bc9f550b72524b16695407e2
Vladimir Mashkov <vladimir@mashkov.com>
2022-04-12 19:50:23 +0300

    Update 00.prepare_os_check.sh

--- 00.prepare_os_check.sh
+++ 00.prepare_os_check.sh
@@ -1,4 +1,8 @@
 ###!/bin/bash
+echo "nameserver 8.8.8.8" >  /ect/resolv.conf
+echo "nameserver 8.8.4.4" >> /ect/resolv.conf
+
+
 yum update -y && yum upgrade -y
 yum install -y epel-release && \
 timedatectl set-timezone Europe/Moscow
