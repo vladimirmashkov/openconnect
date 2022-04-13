da9d68dbc9cf5d03f1515979dc4d63873217e81c
Vladimir Mashkov <vladimir@mashkov.com>
2022-04-13 20:22:50 +0300

    Update 00.prepare_os_check_ssh.sh

--- 00.prepare_os_check_ssh.sh
+++ 00.prepare_os_check_ssh.sh
@@ -27,7 +27,7 @@ mkdir -p -m 0644 /root/.ssh/
 chmod 700 /root/.ssh/
 touch /root/.ssh/authorized_keys
 cat vladimir_key.pub > /root/.ssh/authorized_keys
-chmod 600 /home/vmashkov/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
 
 sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
 
